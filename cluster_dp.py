be3466c1d36f344b08b0cdc21cabc5ad75f6b2b0
yancongwen <ycwen1119@163.com>
2019-10-02 12:09:19 +0800

    fix

--- cluster_dp.py
+++ cluster_dp.py
@@ -25,24 +25,22 @@ def loadDataFromFile(fileName):
 #        lineArr.pop()
         location.append(lineArr)
         lineArr = []
-                
     location = array(location)
     fr.close()
-    
     return location
 
 ### 点坐标系距离计算函数
 def distEclud(vecA, vecB):
-    return sqrt(sum(power(vecA - vecB, 2)))   
+    return sqrt(sum(power(vecA - vecB, 2)))
 
 ### 经纬度距离计算函数
 def distSLC(vecA, vecB):#Spherical Law of Cosines
-    lon1, lat1, lon2, lat2 = map(radians, [vecA[0], vecA[1], vecB[0], vecB[1]])  
-    dlon = lon2 - lon1   
-    dlat = lat2 - lat1   
-    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2  
-    c = 2 * arcsin(sqrt(a))   
-    r = 6378.1 # 地球平均半径，单位为公里  
+    lon1, lat1, lon2, lat2 = map(radians, [vecA[0], vecA[1], vecB[0], vecB[1]])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * arcsin(sqrt(a))
+    r = 6378.1 # 地球平均半径，单位为公里
  
     return c * r * 1000 #pi is imported with numpy
 
@@ -133,18 +131,18 @@ def calDelta(dist, rho):
     length = len(rho)
     delta = zeros(length)
     nneigh = zeros(length)
-    ## 将 rho 按降序排列，ordrho 原rho的索引  
-    ordrho = argsort(-array(rho))     
+    ## 将 rho 按降序排列，ordrho 原rho的索引
+    ordrho = argsort(-array(rho))
 
-    ## 生成 delta 和 nneigh 数组  
-    for i in range(1,length):  
-        delta[ordrho[i]] = inf  
-        for j in range(0,i):  
+    ## 生成 delta 和 nneigh 数组
+    for i in range(1,length):
+        delta[ordrho[i]] = inf
+        for j in range(0,i):
             if dist[ordrho[i],ordrho[j]] < delta[ordrho[i]]:
-                delta[ordrho[i]] = dist[ordrho[i],ordrho[j]]  
-                nneigh[ordrho[i]] = ordrho[j]    
+                delta[ordrho[i]] = dist[ordrho[i],ordrho[j]]
+                nneigh[ordrho[i]] = ordrho[j]
     
-    ## 生成 rho 值最大数据点的 delta 值  
+    ## 生成 rho 值最大数据点的 delta 值
 #    delta[ordrho[0]] = max(dist[ordrho[0],:])
     delta[ordrho[0]] = max(delta)
     nneigh[ordrho[0]] = 0
@@ -167,12 +165,12 @@ def getMark(rho, delta, nneigh, rhomin, deltamin):
             mark[i] = num
             imark.append(i)
             num = num + 1
-    print('NUMBER OF CLUSTERS：', num)
-    ## 将其他数据点归类 (assignation)  
+    print('NUMBER OF CLUSTERS:', num)
+    ## 将其他数据点归类 (assignation)
     print('Performing assignation')
-    for i in range(length):  
+    for i in range(length):
         if mark[ordrho[i]] == -1:
-            mark[ordrho[i]] = mark[nneigh[ordrho[i]]]  
+            mark[ordrho[i]] = mark[nneigh[ordrho[i]]]
 
     return num,mark,imark
 
@@ -187,27 +185,27 @@ def getHalo(mark, nclust, imark, dist, dc):
   
     ## 获取每一个 cluster 中平均密度的一个界 bord_rho  
     for i in range(length): 
-        for j in range(i+1,length):  
+        for j in range(i+1,length):
             if mark[i] != mark [j] and dist[i,j] <= dc: 
                 rho_aver = (rho[i] + rho[j]) / 2.0   #取 i,j 两点的平均局部密度  
-                if rho_aver > bord_rho[mark[i]]:   
-                    bord_rho[mark[i]] = rho_aver  
+                if rho_aver > bord_rho[mark[i]]:
+                    bord_rho[mark[i]] = rho_aver
                 if rho_aver > bord_rho[mark[j]]:
                     bord_rho[mark[j]] = rho_aver
 
     ## 计算离群点 outlier
     for i in range(length):
         if rho[i] < bord_rho[mark[i]]:
-            halo[i] = -1 
-    ## 逐一处理每个 cluster  
+            halo[i] = -1
+    ## 逐一处理每个 cluster
     for i in range(nclust):
-        na = 0    #用于累计当前 cluster 中数据点的个数  
-        nc = 0    #用于累计当前 cluster 中核心数据点的个数  
+        na = 0    #用于累计当前 cluster 中数据点的个数
+        nc = 0    #用于累计当前 cluster 中核心数据点的个数
         for j in range(length):
-            if mark[j] == i:   
-                na = na + 1  
-            if halo[j] == i:   
-                nc = nc + 1 
+            if mark[j] == i:
+                na = na + 1
+            if halo[j] == i:
+                nc = nc + 1
         cord = 'CLUSTER： ' + str(i + 1) + ' CENTER： ' + str(imark[i])
         cord = cord + ' ELEMENTS： ' + str(na) + ' CORE： ' + str(nc) + ' HALO： ' + str(na - nc)
         print(cord)
@@ -216,7 +214,7 @@ def getHalo(mark, nclust, imark, dist, dc):
 
 # In[30]:
 
-def draw(rho, delta, mark):    
+def draw(rho, delta, mark):
     length = len(rho)
     ## 绘制决策图
     plt.figure(1)
@@ -229,7 +227,7 @@ def draw(rho, delta, mark):
     ## 将 rho 按降序排列，ordrho 原rho的索引
     plt.figure(2)
     gama = rho * delta
-    ordgama = argsort(-array(gama)) 
+    ordgama = argsort(-array(gama))
     plt.xlim(-0.06 * length, length)
     plt.plot(range(length), gama[ordgama], 'o')
     plt.title('Gama Graph')
@@ -260,13 +258,13 @@ def draw(rho, delta, mark):
             plt.plot(dataSet[i][0], dataSet[i][1], color = colors[index % 255], marker = 'o')
     plt.title('Cluster Result')
     plt.xlabel('x'), plt.ylabel('y')
-    plt.show()    
+    plt.show()
 
 
 # In[31]:
 
 #加载数据
-dataSet = loadDataFromFile('data/beijingcmi.txt')
+dataSet = loadDataFromFile('data/11.txt')
 #print(dataSet)
 #计算或加载数据集的距离矩阵、阈值dc
 dist,dc = getDist(dataSet, 0.02)
@@ -283,7 +281,7 @@ delta, nneigh = calDelta(dist, rho)
 #设置参数
 #rhomin = 4.5565; deltamin = 4.0873    #Compound   nclust = 5
 #rhomin = 5.0403; deltamin = 5.9127    #Compound   nclust = 4
-rhomin = 50; deltamin = 0.8    #Spiral
+rhomin = 2; deltamin = 1    #Spiral
 #rhomin = 4.5323; deltamin = 6.5873    #Flame
 #rhomin = 39.1935; deltamin = 1.4286    #D31
 
